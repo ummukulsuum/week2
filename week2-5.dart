c9797313d15e1c7367a6672cac8422617bf0bcfb
ummukulsuum <sajidabee82@gmail.com>
2025-07-23 12:11:08 +0530

    Create week2-5.dart

---
+++ week2-5.dart
@@ -0,0 +1,17 @@
+class Intern{
+  String name="Arun";
+  String age ="Twenty";
+  String place="Kakkanjery";
+  String bg="BCA";
+  String domain="DataAnalytics";
+  String review="Day-6";
+}
+void main(){
+Intern std=Intern();
+  print( std.name);
+  print(std.age);
+  print(std.place);
+  print(std.domain);
+  print(std.bg);
+  print(std.review);
+}
