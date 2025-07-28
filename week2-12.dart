2e1ddfc747baf330dbf015f0bd7b036d3d1c5d9e
ummukulsuum <sajidabee82@gmail.com>
2025-07-28 17:54:01 +0530

    Create week2-12.dart

---
+++ week2-12.dart
@@ -0,0 +1,12 @@
+class Pets {
+  void mythical(){
+    print("unicorn 🦄!");
+  }
+  void bleat(){
+    print("panda 🐼!");
+  }
+}
+void main(){
+  Pets sounds =Pets();
+sounds..mythical()..bleat();
+}
