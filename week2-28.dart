4df413695f1e5c8a85e2839d46c7efdc1b4dd8bb
ummukulsuum <sajidabee82@gmail.com>
2025-07-31 12:25:06 +0530

    Create week2-28.dart

---
+++ week2-28.dart
@@ -0,0 +1,16 @@
+void main(){
+ int rank=1;
+  switch(rank){
+    case 1:
+    print("first");
+    break;
+    case 2:
+    print("second");
+    break;
+    case 3:
+    print("third");
+    break;
+    default:
+    print("fourth");
+  }
+}
